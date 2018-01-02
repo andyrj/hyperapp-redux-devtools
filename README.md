d92fefb61dc25d2b73fcd88e075726911457ddc9
Andy Johnson <andyrjohnson82@gmail.com>
2018-01-02 09:05:51 -0800

    updated to hyperapp-1.0 HOA

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ hyperapp HOA(higher order app) to utilize redux-devtools-extension from hyperapp
 import { h, app } from 'hyperapp';
 import devtools from 'hyperapp-redux-devtools';
 
-app(devtools({
+devtools(app)({
   state: { count: 0 },
   view: (state, actions) => {
     return (
@@ -18,6 +18,6 @@ app(devtools({
   actions: {
     increment: (state) => Object.assign({}, state, { count: state.count + 1 })
   }
-}));
+});
 
 ```
--- index.js
+++ index.js
@@ -1,112 +1,41 @@
 var { createStore } = require("redux");
 var { composeWithDevTools } = require("redux-devtools-extension");
 
-module.exports = function devtools(props) {
-  function reducer(state = {}, action) {
-    return Object.assign({}, state, action.payload);
-  }
+function reducer(state = {}, action) {
+  return Object.assign({}, state, action.payload);
+}
 
-  function action(name, data) {
-    return {
-      type: name,
-      payload: data
-    };
-  }
-  
+function action(name, data) {
+  return {
+    type: name,
+    payload: data
+  };
+}
+
+module.export = function devtools(app) {
   var composeEnhancers = composeWithDevTools({ action: action });
-  
   var store;
-  var firedActions = [];
+  var inAction = false;
 
-  var devtoolsProps = {
-    actions: {
-      replaceState: function(state) {
-        return store.getState();
+  return function(state, actions, view, container) {
+    Object.keys(actions || {}).forEach(key => {
+      const action = actions[key];
+      actions[key] = () => {
+        inAction = true;
+        const result = action.apply(this, arguments);
+        store.dispatch(action(key, result));
+        inAction = false;
       }
-    },
-    hooks: [
-      function (state, actions) {
-        // equivalent to events.load...
-        store = createStore(reducer, state, composeEnhancers());
-        store.subscribe(function() {
-          actions.replaceState(store.getState());
-        });
-        return function(info) {
-          // equivalent to events.actions
-          if (info.name !== "replaceState") {
-            firedActions.push(info.name);
-          }
-          return function(result) {
-            //equivalent to events.resolve...
-            if (typeof result === "function") {
-              const action = firedActions.pop()
-              return update => {
-                result(updateResult => {
-                  firedActions.push(action)
-                  update(updateResult)
-                })
-              }
-            }
-            // return result; // not sure how to return result here and also handle update hook, maybe not needed...?
-            return function(data) {
-              // equivalent to events.update
-              if (firedActions.length > 0 && store !== undefined) {
-                var action = firedActions.pop();
-                if (action !== "replaceState") {
-                  store.dispatch({ type: action, payload: data });
-                }
-              }
-              return data;
-            }
-          }
-        }
-      }
-    ]
-  };
-
-  return Object.assign(props, {
-    actions: devtoolsProps.actions,
-    hooks: props.hooks.concat(devtoolsProps.hooks)
-  });
-  /*
-  return function(app) {
-    var mixin = {
-      actions: {
-        
-      },
-      events: {
-        load: function(state, actions, root) {
-          
-        },
-        action: function(state, actions, info) {
-          
-        },
-        resolve(state, actions, result) {
-          // how to re-write this...
-          if (typeof result === "function") {
-            const action = firedActions.pop()
-            return update => {
-              result(updateResult => {
-                firedActions.push(action)
-                update(updateResult)
-              })
-            }
-          }
-          return result;
-        },
-        update: function(state, actions, data) {
-          if (firedActions.length > 0 && store !== undefined) {
-            var action = firedActions.pop();
-            if (action !== "replaceState") {
-              store.dispatch({ type: action, payload: data });
-            }
-          }
-          return data;
-        }
-      }
+    });
+    actions.replaceState = () => (state, actions) => {
+      return store.getState();
     };
-
-    return mixin;
-  };
-  */
+    store = createStore(reducer, state, composeEnhancers());
+        store.subscribe(function() {
+          if (!inAction) {
+            actions.replaceState(store.getState());
+          }
+        });
+    return app(state, actions, view, container);
+  }
 };
