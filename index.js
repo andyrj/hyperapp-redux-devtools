eb386e0ade5dd90e3d64b37b1775e38957b51d7c
Andy Johnson <andyrjohnson82@gmail.com>
2018-01-22 13:55:07 -0800

    Revert "fix problem with nested state and actions (#9)"
    This reverts commit 75ee1d54c0ad4c0c192242f7f3e412dc187259e7.

--- index.js
+++ index.js
@@ -1,73 +1,43 @@
 var { createStore } = require("redux");
 var { composeWithDevTools } = require("redux-devtools-extension");
 
-function reduxReducer(state = {}, action) {
+function reducer(state = {}, action) {
   return Object.assign({}, state, action.payload);
 }
 
-function reducAction(name, data) {
+function action(name, data) {
   return {
     type: name,
     payload: data
   };
 }
 
-function copy(target, source) {
-  var obj = {}
-  for (var i in target) obj[i] = target[i]
-  for (var i in source) obj[i] = source[i]
-  return obj
-}
-
-function set(path, value, source, target) {
-  if (path.length) {
-    target[path[0]] =
-      1 < path.length ? set(path.slice(1), value, source[path[0]], {}) : value
-    return copy(source, target)
-  }
-  return value
-}
-
 module.exports = function devtools(app) {
-  var composeEnhancers = composeWithDevTools({ action: reducAction });
+  var composeEnhancers = composeWithDevTools({ action: action });
   var store;
 
   return function(state, actions, view, container) {
     var appActions;
-
-    function wire(path, actions) {
-      for (var key in actions) {
-        if (typeof actions[key] === "function") {
-          (function(key, action) {
-            actions[key] = function() {
-              var reducer = action.apply(this, arguments);
-              return function (slice) {
-                var data = reducer(slice, appActions);
-                if (data && !data.then) {
-                  state = set(path, copy(slice, data), state, {});
-                  store.dispatch(reducAction(key, state));
-                }
-                return data;
-              };
-            };
-          })(key, actions[key]);
-        } else {
-          wire(path.concat(key), (actions[key] = copy(actions[key])));
-        }
-      }
-    }
-    wire([], (actions = copy(actions)));
-
+    Object.keys(actions || {}).forEach(function (key) {
+      var act = actions[key];
+      actions[key] = function() {
+        var reducer = act.apply(this, arguments);
+        return function (state) {
+          var newState = typeof reducer === "function" ? reducer(state, appActions) : reducer;
+          store.dispatch(action(key, newState));
+          return newState;
+        };
+      };
+    });
     actions.replaceState = function(actualState) {
       return function (state) {
         return actualState;
       }
     };
-    store = createStore(reduxReducer, state, composeEnhancers());
+    store = createStore(reducer, state, composeEnhancers());
     store.subscribe(function() {
       appActions.replaceState(store.getState());
     });
-
     appActions = app(state, actions, view, container);
     return appActions;
   };
