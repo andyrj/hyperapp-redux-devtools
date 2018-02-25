a5e03cd4d94b530ee81e6289998c65365e72fdf4
Andy Johnson <andyrjohnson82@gmail.com>
2018-02-25 00:07:38 -0800

    fix: bumping version for bug introduced by 1.1.5 for root actions, now it should be working for both root and nested recieving proper actions in their execution after being wrapped

--- index.js
+++ index.js
@@ -13,19 +13,26 @@ function reducAction(name, data) {
 }
 
 function copy(target, source) {
-  var obj = {}
-  for (var i in target) obj[i] = target[i]
-  for (var i in source) obj[i] = source[i]
-  return obj
+  var obj = {};
+  for (var i in target) obj[i] = target[i];
+  for (var i in source) obj[i] = source[i];
+  return obj;
 }
 
 function set(path, value, source, target) {
   if (path.length) {
     target[path[0]] =
-      1 < path.length ? set(path.slice(1), value, source[path[0]], {}) : value
-    return copy(source, target)
+      1 < path.length ? set(path.slice(1), value, source[path[0]], {}) : value;
+    return copy(source, target);
   }
-  return value
+  return value;
+}
+
+function get(path, source) {
+  for (var i = 0; i < path.length; i++) {
+    source = source[path[i]];
+  }
+  return source;
 }
 
 module.exports = function devtools(app) {
@@ -42,7 +49,9 @@ module.exports = function devtools(app) {
             actions[key] = function() {
               var reducer = action.apply(this, arguments);
               return function (slice) {
-                var data = typeof reducer === "function" ? reducer(slice, appActions[path]) : reducer;
+                var data = typeof reducer === "function"
+                  ? reducer(slice, get(path, appActions))
+                  : reducer;
                 if (data && !data.then) {
                   state = set(path, copy(slice, data), state, {});
                   store.dispatch(reducAction(key, state));
